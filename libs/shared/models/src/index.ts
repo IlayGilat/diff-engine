1bcb9fd6ac7a30920ec4eeb42dc57814780acbde
Ilay Gilat <gilat.ilay@gmail.com>
2026-03-27 18:39:36 +0300

    Remove unused shop and api workspace projects

--- libs/shared/models/src/index.ts
+++ libs/shared/models/src/index.ts
@@ -2,5 +2,4 @@ export * from './lib/demo-polling-domain.model';
 export * from './lib/domains/activity-state.model';
 export * from './lib/domains/overview-state.model';
 export * from './lib/json-value.model';
-export * from './lib/product.model';
 export * from './lib/realtime-polling.model';
